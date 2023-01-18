39f13ea0e49b2c6a4944d6527968cd5beaf6bd16
PeacefulDog <nipas.devs@gmail.com>
2023-01-18 11:21:51 +0700

    chore(app) : remove all comment in init code

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -5,18 +5,18 @@ export default class AppProvider {
 	}
 
 	public register() {
-		// Register your own bindings
+
 	}
 
 	public async boot() {
-		// IoC container is ready
+
 	}
 
 	public async ready() {
-		// App is ready
+
 	}
 
 	public async shutdown() {
-		// Cleanup, since app is going down
+
 	}
 }
