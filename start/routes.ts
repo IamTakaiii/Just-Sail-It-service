d27f8d74db9488515f453a7d0cfa6d8189e0aab8
PeacefulDogs <nipas.devs@gmail.com>
2023-01-30 19:10:44 +0700

    fix : remove middleware from get profile

--- start/routes.ts
+++ start/routes.ts
@@ -4,7 +4,6 @@ Route.group(() => {
 	Route.post('/register', 'UsersController.create')
 		.middleware(['ReqRegister'])
 	Route.get('/profile/:id', 'UsersController.getUserProfile')
-		.middleware(['AuthGetData'])
 	Route.patch('/profile/update/:id', 'UsersController.updateUserProfile')
 		.middleware(['AuthUpdateData', 'ReqUpdateProfile'])
 	Route.delete('/delete/:id', 'UsersController.delete')
