ff93cf88d6f0d565629a174255151e85817b9d13
PeacefulDog <nipas.devs@gmail.com>
2023-02-02 14:47:25 +0700

    fix(image): fix img not found bug when send other image key to upload img-profile
    1. add cover & image condition - if not found send response to user

--- app/Middleware/ReqUploadImg.ts
+++ app/Middleware/ReqUploadImg.ts
@@ -8,13 +8,30 @@ export default class ReqUploadImg {
 			profile_image: schema.file.optional({
 				size: '50mb',
 				extnames: ['jpg', 'gif', 'png']
+			}),
+			cover_image: schema.file.optional({
+				size: '50mb',
+				extnames: ['jpg', 'gif', 'png']
 			})
 		})
 	}
 
 	public async handle({ request, response }: HttpContextContract, next: () => Promise<void>) {
-		if (JSON.stringify(request.file) === '{}' || JSON.stringify(request.files) === '{}') response.status(400).send({ status: false, error: 'empty body' })
-		await request.validate({ schema: this.schema() })
-		await next()
+		const cover_image = request.file('cover_image')
+		const image = request.file('profile_image')
+		if (!cover_image && !image) {
+			response.status(400).send({
+				status: false,
+				error: "Can't find cover_image or profile_image key in request object",
+				payload: null
+			})
+		}
+		else if (JSON.stringify(request.file) === '{}' || JSON.stringify(request.files) === '{}') {
+			response.status(400).send({ status: false, error: 'empty body', payload: null })
+		}
+		else {
+			await request.validate({ schema: this.schema() })
+			await next()
+		}
 	}
 }
