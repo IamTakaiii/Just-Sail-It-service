991173e9ab403eaeff1e15b2bfa1561e1a46e1fc
PeacefulDogs <nipas.devs@gmail.com>
2023-02-14 17:16:22 +0700

    init : clean old version and Init new version

--- start/redis.ts
+++ start/redis.ts
@@ -1,11 +1,2 @@
 import Redis from '@ioc:Adonis/Addons/Redis'
 import Event from '@ioc:Adonis/Core/Event'
-
-import { EVENTDB } from 'App/Constants/EventCode'
-
-Redis.psubscribe('project:*', (topic: string, message: string) => {
-	console.log('message from event listenner has arrive !!!')
-	if (topic === 'project:create_done') Event.emit(EVENTDB.SAVE_PROJECT, message)
-	if (topic === 'project:donate_done') console.log(message)
-	if (topic === 'project:withdraw_done') console.log(message)
-})
