3ab675ea60fb5b097ce8555a07785a61d7981a76
PeacefulDogs <nipas.devs@gmail.com>
2023-02-14 17:14:38 +0700

    init : clean old version and Init new version

--- start/kernel.ts
+++ start/kernel.ts
@@ -1,16 +1,7 @@
 import Server from '@ioc:Adonis/Core/Server'
 
 Server.middleware.register([
-	() => import('@ioc:Adonis/Core/BodyParser'),
 ])
 
 Server.middleware.registerNamed({
-	ValidateAuth: () => import('App/Middleware/Auth/ValidateAuth'),
-	ValidateAuthUpdate: () => import('App/Middleware/Auth/ValidateAuthUpdate'),
-	ValidateAuthUserUpdate: () => import('App/Middleware/Auth/ValidateAuthUserUpdate'),
-	ValidateRegister: () => import('App/Middleware/User/ValidateRegister'),
-	ValidateUpdateUser: () => import('App/Middleware/User/ValidateUpdateUser'),
-	ValidateLogin: () => import('App/Middleware/Auth/ValidateLogin'),
-	ValidateCreateProject: () => import('App/Middleware/Project/ValidateCreateProject'),
-	ValidateUpdateProject: () =>import('App/Middleware/Project/ValidateUpdateProject')
 })
--- start/routes.ts
+++
@@ -1,4 +0,0 @@
-import 'App/Routes/user_routes'
-import 'App/Routes/auth_routes'
-import 'App/Routes/image_routes'
-import 'App/Routes/project_routes'
